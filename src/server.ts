f3838befd95ca2debb66379060d1a2cb36cc36b4
Liran Cohen <iliran11@gmail.com>
2020-02-15 12:05:40 +0200

    include healthcheck

--- src/server.ts
+++ src/server.ts
@@ -1,16 +1,19 @@
-import moment from 'moment';
-import helloworld from './hello';
-import express, { Response, Request } from 'express';
-import bodyParser from 'body-parser';
+import moment from "moment";
+import helloworld from "./hello";
+import express, { Response, Request } from "express";
+import bodyParser from "body-parser";
 const app = express();
 const port = 9000;
 
 app.use(bodyParser.json());
-app.post('/', async (req: Request, res: Response) => {
+app.post("/", async (req: Request, res: Response) => {
   const startTime = moment();
   const scores = await helloworld(req.body.site);
   const time = moment().diff(startTime) / 1000;
   res.json({ time, scores });
+});
+app.get("/healthcheck", (_req: Request, res: Response) => {
+  res.json({ status: "ok", time: Date.now() });
 });
 
 app.listen(port, () =>
